62a50933b0c60d84dab03013c73174cb145880a4
Maksimka <mister.max1988@yandex.ru>
2026-05-30 19:51:56 +0500

    Homework 2: CBV в catalog, приложение blog и CRUD, так же внес изменения в README.md

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,15 @@
+"""Маршруты приложения catalog."""
+
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, home
+from catalog.views import ContactView, ProductDetailView, ProductListView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("home/", home, name="home_page"),
-    path("contacts/", contacts, name="contacts"),
+    path("", ProductListView.as_view(), name="home"),
+    path("home/", ProductListView.as_view(), name="home_page"),
+    path("contacts/", ContactView.as_view(), name="contacts"),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,49 @@
-from django.shortcuts import render
+"""Контроллеры приложения catalog."""
+
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.views import View
+from django.views.generic import DetailView, ListView
+
+from catalog.forms import ContactForm
+from catalog.models import Product
 
 
-def home(request):
-    """Отображает главную страницу каталога."""
-    return render(request, "catalog/home.html")
+class ProductListView(ListView):
+    """Отображает главную страницу со списком товаров."""
+
+    model = Product
+    template_name = "catalog/home.html"
+    context_object_name = "products"
+
+    def get_queryset(self):
+        """Возвращает все товары каталога."""
+        return Product.objects.all()
 
 
-def contacts(request):
-    """Отображает страницу контактов."""
-    return render(request, "catalog/contacts.html")
+class ProductDetailView(DetailView):
+    """Отображает страницу с подробной информацией о товаре."""
+
+    model = Product
+    template_name = "catalog/product_detail.html"
+    context_object_name = "product"
+
+
+class ContactView(View):
+    """Отображает страницу контактов и обрабатывает форму обратной связи."""
+
+    template_name = "catalog/contacts.html"
+
+    def get(self, request):
+        """Отображает форму обратной связи."""
+        return render(request, self.template_name, {"form": ContactForm()})
+
+    def post(self, request):
+        """Проверяет и сохраняет сообщение из формы."""
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Сообщение успешно отправлено.")
+            return redirect("catalog:contacts")
+        messages.error(request, "Исправьте ошибки в форме.")
+        return render(request, self.template_name, {"form": form})
